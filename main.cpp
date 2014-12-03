2f02ab101b18993d53f2a61c55a09732bfd63452
Binomi <felix.kurtz@stud.uni-goettingen.de>
2014-12-03 22:53:38 +0100

    Bugs behoben, Verteilung randomPlayer 41.2:36.7:22.1

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int field[11][9];
 int field_pos=-1;
 int fieldsDecided=0;
 bool isActive=true;
-int wonGames[3]={0,0,0};
+int wonGames[3];//={0,0,0};
 int playedGames=0;
 
 void reset () {
@@ -24,16 +24,16 @@ void reset () {
 }
 
 void printField() {
-/*		cout << "----------------------------------------------" << endl;	
+    /*		cout << "----------------------------------------------" << endl;
     for(int i=0; i<3; i++) {
-	for(int j=0;j<3; j++){
-		cout <<"| " << field[i*3][j*3] << "  " << field[i*3][j*3+1] << "  " << field[i*3][j*3+2] << "\t| " << field[i*3+1][j*3] << "  " <<field[i*3+1][j*3+1] << "  " <<field[i*3+1][j*3+2] << "\t| " << field[i*3+2][j*3] << "  " << field[i*3+2][j*3+1] << "  " << field[i*3+2][j*3+2] << " |" << endl;
-	}
-		cout << "----------------------------------------------" << endl;
+    for(int j=0;j<3; j++){
+        cout <<"| " << field[i*3][j*3] << "  " << field[i*3][j*3+1] << "  " << field[i*3][j*3+2] << "\t| " << field[i*3+1][j*3] << "  " <<field[i*3+1][j*3+1] << "  " <<field[i*3+1][j*3+2] << "\t| " << field[i*3+2][j*3] << "  " << field[i*3+2][j*3+1] << "  " << field[i*3+2][j*3+2] << " |" << endl;
+    }
+        cout << "----------------------------------------------" << endl;
 */
-	    //}
-	    //cout << " | ";
-	/*cout << i << ": ";
+    //}
+    //cout << " | ";
+    /*cout << i << ": ";
         for(int j=0; j<9; j++) {
             cout << field[i][j] << "  ";
         }
@@ -42,7 +42,7 @@ void printField() {
     
     cout << "Spieler 1: " <<wonGames[0] << " Spieler -1: " << wonGames[1] << " Unentschieden: " << wonGames[2] << endl;
     for(int i=0;i<3;i++){
-	cout << field[9][i*3] << " | " << field[9][i*3+1] << " | " << field[9][i*3+2] << endl;
+        cout << field[9][i*3] << " | " << field[9][i*3+1] << " | " << field[9][i*3+2] << endl;
     }
 }
 
@@ -74,18 +74,17 @@ void tie(int pos) {
 bool set(int player, int pos) {
     if(field[field_pos][pos]==0) {
         field[field_pos][pos]=player;
-         field[10][field_pos]++;
+        field[10][field_pos]++;
     } else {return false;}
     int result=victory(field[field_pos]);
-    if (result==0){wonGames[2]+=1;}
     if (result!=0) {
         field[9][field_pos]=result;
         fieldsDecided++;
         int overall_vic=victory(field[9]);
         if(overall_vic!=0) {
-		if (overall_vic==1){
-			wonGames[0]+=1;
-		} else {wonGames[1]+=1;}
+            if (overall_vic==1){
+                wonGames[0]++;
+            } else {wonGames[1]++;}
 
             //cout << "Spieler " << overall_vic << " hat das gesamte Spiel gewonnen." << endl;
             isActive=false;
@@ -114,23 +113,32 @@ void randomPlayer(int player) {
 
 int main()
 {
-    while (playedGames<1e5) {
-	playedGames++;
-    	srand(time(0));
-    	reset();
-   	int k=0;
-    	int player=1;
-	int kOld=k;
-    	while(isActive && (fieldsDecided<9)) {
-        	randomPlayer(player);
-        	k++;
-        	player*=-1;
-    	}
-	if (kOld != k){
-    		cout << playedGames << "\t" << wonGames[0] << "\t" << wonGames[1] << "\t" << k << endl;
-	}
-    	//printField();
-    //sleep(1);
+    srand(time(NULL));
+    int k,kOld;
+    while (playedGames<1e6) {
+        // PC ist zu schnell um verschiedene seeds zu setzen
+        // Ausgabe viel langsamer als eigentlicher Rechenprozess
+        //srand(time(NULL));
+
+        playedGames++;
+
+        reset();
+        kOld=k;
+        k=0;
+
+        int player=1;
+
+        while(isActive && (fieldsDecided<9)) {
+            randomPlayer(player);
+            k++;
+            player*=-1;
+        }
+        if(isActive) {
+            wonGames[2]++;
+        }
+        if (kOld != k){
+            //cout << playedGames << "\t" << wonGames[0] << "\t" << wonGames[1] << "\t" << k << endl;
+        }
     }
     printField();
     return 0;
