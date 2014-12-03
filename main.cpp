80c6b8e0bda2cb4b2285f5cb94631413fd363ec0
binomi <felix.kurtz@stud.uni-goettingen.de>
2014-12-03 15:58:10 +0100

    fast lauffertig, aber komische Dinge passieren

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int field[11][9];
 int field_pos=-1;
+int fieldsDecided=0;
+bool isActive=true;
 
 void reset () {
     for(int i=0; i<sizeof(field)/sizeof(field[0]); i++) {
@@ -11,6 +14,18 @@ void reset () {
             field[i][j]=0;
         }
     }
+}
+
+void printField() {
+    for(int i=0; i<11; i++) {
+        cout << i << ": ";
+        for(int j=0; j<9; j++) {
+            cout << field[i][j] << "  ";
+        }
+        cout << endl;
+
+    }
+    cout << endl;
 }
 
 int victory (int arr[]) {
@@ -43,22 +58,46 @@ bool set(int player, int pos) {
     int result=victory(field[field_pos]);
     if (result!=0) {
         field[9][field_pos]=result;
-        int overall_vic=victory(field(9));
+        fieldsDecided++;
+        printField();
+        int overall_vic=victory(field[9]);
         if(overall_vic!=0) {
-            cout << "Spieler " + overall_vic + " hat das gesamte Spiel gewonnen." << endl;
+            cout << "Spieler " << overall_vic << " hat das gesamte Spiel gewonnen." << endl;
+            isActive=false;
         }
     } else {tie(field_pos);}
 
-    if (field[9][field_pos]!=0){field_pos=pos;}
+    if (field[9][field_pos]==0){field_pos=pos;}
     else {field_pos=-1;}
 
     return true;
 }
 
+void randomPlayer(int player) {
+    if(field_pos==-1) {
+        int newFP;
+        do {
+            newFP = rand()%9;
+        } while(field[9][newFP]!=0);
+        field_pos=newFP;
+    }
+    int pos=rand()%9;
+    while(!set(player, pos)) {
+        pos=rand()%9;
+    }
+}
 
 int main()
 {
-
+    int k=0;
+    int player=1;
+    while(isActive && (fieldsDecided<9)) {
+        randomPlayer(player);
+        k++;
+        player*=-1;
+    }
+    cout << k << endl;
+    printField();
     return 0;
 }
 
